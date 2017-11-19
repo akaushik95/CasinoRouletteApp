189f7fedced2e2dc71f0bfb97fad9cd7a2e3f360
akaushik95 <ashukaushik8395@gmail.com>
2017-11-19 22:07:13 +0530

    final commit v2

--- CasinoAdmin/src/main/webapp/resources/js/script.js
+++ CasinoAdmin/src/main/webapp/resources/js/script.js
@@ -1,11 +1,4 @@
 console.log("Script is running");
 window.onload = function(){
-	function checkDate(){
-		var x=document.getElementById("dob");
-		console.log("helloooooooo"+x);
-		/* var date = document.getElementById("dob").value;
-		console.log(date);
-		var d = new Date();
-		console.log(d); */
-	}
+	console.log("script is running !!!!");
 }
